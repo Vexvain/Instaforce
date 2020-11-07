68e20e3ef5d33aecbb2e0856b95b58546eeed9ef
Vexvain <60254325+Vexvain@users.noreply.github.com>
2020-11-06 21:05:16 -0500

    Update test_proxies.py

--- test_proxies.py
+++ test_proxies.py
@@ -1,8 +1,6 @@
 '''
-Date: 3/15/2019
-Author: Mohamed
-Description: Reads a file that contains a list of proxies and determines whether or not that list is good.
-             Each line in the file must be in the format of ip:port
+VEXVAIN 
+
 '''
 
 import platform
